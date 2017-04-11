c9be936f624cdad64e740af21888edce480a9649
Ubuntu <ubuntu@ip-172-31-27-238.ec2.internal>
2017-04-11 18:11:19 +0000

    Add log file and dropdown

--- server.py
+++ server.py
@@ -1,12 +1,38 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    files = []
+    for file in os.listdir("static/bridgekmzs"):
+        if file.endswith(".kmz"):
+            files.append(file)
+    kmz = files[len(files)-1]
+    logfile = str(kmz).split(".")[0] + ".txt"
+    return render_template('index.html', kmz = kmz, files = files, logfile = logfile)
 
+@app.route('/view/<kmz>')
+def view(kmz):
+    files = []
+    for file in os.listdir("static/bridgekmzs"):
+        if file.endswith(".kmz"):
+            files.append(file)
+    logfile = str(kmz).split(".")[0] + ".txt"
+    return render_template('index.html', kmz = str(kmz), files = files, logfile = logfile)
+
+
+@app.route('/log/<logfile>')
+def log(logfile):
+    files = []
+    for file in os.listdir("static/bridgekmzs"):
+        if file.endswith(".kmz"):
+            files.append(file)
+    with open("static/logs/" + logfile, "r") as f:
+        content = f.read()
+    return render_template('log.html', content = content, files = files)
 if __name__ == "__main__":
     app.run()
